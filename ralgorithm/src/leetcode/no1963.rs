7cd3a9d45fa259541839d90d3b36bbd683a147ec
manqing321 <manqing321@gmail.com>
2025-03-17 11:45:42 +0800

    update answer of no1963

--- ralgorithm/src/leetcode/no1963.rs
+++ ralgorithm/src/leetcode/no1963.rs
@@ -3,9 +3,8 @@ pub struct Solution;
 impl Solution {
     pub fn min_swaps(s: String) -> i32 {
         let mut diff = 0;
-        for idx in 0..s.len() {
-            let ch = s.get(idx..idx + 1);
-            if let Some("[") = ch {
+        for ch in s.chars() {
+            if ch == '[' {
                 diff += 1;
             } else if diff > 0 {
                 diff -= 1;
