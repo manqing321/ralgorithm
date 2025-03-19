303fdc4b0dfd68adba33df85023d635debf710db
manqing321 <manqing321@gmail.com>
2025-03-19 11:38:13 +0800

    add answer of 2610

---
+++ ralgorithm/src/leetcode/no2610.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+
+
+pub struct Solution;
+
+impl Solution {
+    pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut ans = Vec::new();
+        let mut mp:HashMap<i32, i32> = HashMap::new();
+        for num in nums {
+            mp.entry(num).and_modify(|num| * num += 1).or_insert(1);
+        }
+        while !mp.is_empty() 
+        {
+            let mut vector:Vec<i32> = Vec::new();
+            for num in mp.keys() 
+            {
+                vector.push(*num);
+            }
+            for num in vector.iter()
+            {
+                mp.entry(*num).and_modify(|num| *num -= 1);
+                if mp[num] <= 0
+                {
+                    mp.remove(num);
+                }
+            }
+            ans.push(vector);
+        }
+        return ans;
+    }
+}
